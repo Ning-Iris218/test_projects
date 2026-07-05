0daf20dc5689747cfb5b8b314a1314a6f6dc5fe9
ning <linbaijiu@gmail.com>
2026-07-05 11:59:33 -0400

    change

--- Project1/information.py
+++ Project1/information.py
@@ -1,7 +1,8 @@
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
 
 class Information_Page:
@@ -15,31 +16,27 @@ class Information_Page:
         self.wait=WebDriverWait(driver,10)
 
     def fill_info(self, first_name, last_name, postal_code):
-        time.sleep(1)
+        time.sleep(2)
+
         fn = self.wait.until(EC.visibility_of_element_located(self._FIRST_NAME))
-        fn.click()
-        fn.clear()
-        fn.send_keys(first_name)
-
         ln = self.wait.until(EC.visibility_of_element_located(self._LAST_NAME))
-        ln.click()
-        ln.clear()
-        ln.send_keys(last_name)
-
         pc = self.wait.until(EC.visibility_of_element_located(self._POST_CODE))
-        pc.click()
-        pc.clear()
-        pc.send_keys(postal_code)
+
+        actions = ActionChains(self.driver)
+
+        actions.move_to_element(fn).click().send_keys(first_name).pause(0.5) \
+            .move_to_element(ln).click().send_keys(last_name).pause(0.5) \
+            .move_to_element(pc).click().send_keys(postal_code).perform()
+
+        time.sleep(1)
 
     def click_continue(self):
-        try:
-            btn = self.driver.find_element(*self._CONTINUE_BUTTON)
-            self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", btn)
-            time.sleep(0.5)
-            btn.click()
-            self.wait.until(EC.url_contains("step-two"))
+        btn = self.wait.until(EC.presence_of_element_located(self._CONTINUE_BUTTON))
 
+        self.driver.execute_script("arguments[0].click();", btn)
+
+        try:
+            self.wait.until(EC.url_contains("step-two"))
         except TimeoutException:
-            self.driver.save_screenshot("github_error_screenshot.png")
-            print("🚨 警告：等待跳转 step-two 超时！页面截图已保存为 github_error_screenshot.png")
+            self.driver.save_screenshot("despair_screenshot.png")
             raise
